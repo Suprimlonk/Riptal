ef159405b167b9f7fff8f501357a8cfcd34ae245
Marco Aurelio Tamez Robles <omenlap12@gmail.com>
2026-05-01 20:35:28 -0600

    Solucion de error simple con el disparo

--- Assets/Player/Bullet.cs
+++ Assets/Player/Bullet.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float damage = 10f; // Daño que aplica la bala
     public float lifetime = 2f; // Tiempo de vida antes de destruirse
 
     void Start()
@@ -10,17 +9,8 @@ public class Bullet : MonoBehaviour
         // Destruir la bala después de su lifetime
         Destroy(gameObject, lifetime);
     }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        // Aplicar daño si colisiona con un enemigo
-        HealthManager health = collision.gameObject.GetComponent<HealthManager>();
-        if (health != null)
-        {
-            health.TakeDamage(damage); // Asume que HealthManager tiene un método TakeDamage
-        }
-
-        // Destruir la bala al impactar
-        Destroy(gameObject);
+    void OnCollisionEnter(Collision other) {
+        destroy(gameObject); // Destruir la bala al colisionar con cualquier objeto
+        
     }
 }
